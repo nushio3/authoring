46d39f3d29f8866af40495e6a5150392f8f1e3f8
Takayuki Muranushi <muranushi@gmail.com>
2013-09-28 16:33:53 +0900

    aa

--- src/Text/Authoring/Class.hs
+++ src/Text/Authoring/Class.hs
@@ -3,12 +3,14 @@
 
 module Text.Authoring.Class where
 
+import           Control.Lens
 import           Control.Monad.RWS
 import           Control.Monad.State
 import           Control.Monad.Writer
 import           Data.Default (def)
 import           Text.Authoring.Document
 import           Text.Authoring.State
+import           Text.LaTeX (LaTeX)
 import qualified Text.CSL.Input.Identifier.Internal as Citation
 
 -- | The all-in-one environ for writing paper.  
@@ -18,5 +20,7 @@ type MonadAuthoring s w m = (MonadState s m, HasAuthorState s, Citation.HasDatab
 
 type AuthoringT = RWST () Document AuthorState 
 
-runAuthoringT :: AuthoringT m a -> m (a, AuthorState, Document)
-runAuthoringT prog = runRWST prog () def
+runAuthoringT :: Monad m => AuthoringT m a -> m (a, AuthorState, LaTeX)
+runAuthoringT prog = do 
+  (a,s,w) <- runRWST prog () def
+  return (a,s,w ^. latexSrc)
