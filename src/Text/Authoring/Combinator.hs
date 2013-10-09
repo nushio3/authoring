d6b50401b58f8198096fc907273bc043efd96528
nushio <nushio@nushiohshio>
2013-10-09 15:14:55 +0000

    add an module that exports the set of combinators.

---
+++ src/Text/Authoring/Combinator.hs
@@ -0,0 +1,14 @@
+module Text.Authoring.Combinator (
+  
+module Text.Authoring.Combinator.Cite,
+module Text.Authoring.Combinator.Meta,
+module Text.Authoring.Combinator.Ref,
+module Text.Authoring.Combinator.Writer
+                                 
+                                 
+                                 ) where
+
+import Text.Authoring.Combinator.Cite
+import Text.Authoring.Combinator.Meta
+import Text.Authoring.Combinator.Ref
+import Text.Authoring.Combinator.Writer
