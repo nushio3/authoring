bd8bcebfcdbdfa614013aa480611ffd83443f8f4
nushio <nushio@nushiohshio>
2013-10-09 15:32:36 +0000

    released version 0.1 .

--- src/Text/Authoring/Bibliography.hs
+++ src/Text/Authoring/Bibliography.hs
@@ -1,9 +1,20 @@
 module Text.Authoring.Bibliography where
 
+import           Control.Lens (use)
+import           Control.Monad
 import           Control.Monad.State
+import           Control.Monad.IO.Class
+import qualified Data.Set as Set
 import qualified Data.Text as Text
+import           Text.CSL.Input.Identifier (toBibTeXItem, HasDatabase)
 
-import qualified Text.Authoring.State
+import Text.Authoring.State
 
-bibliographyContent :: (MonadState s m, HasAuthorState s) => m Text.Text
-bibliographyContent = undefined
+-- | generate the content for the bibliography file.
+
+bibliographyContent :: (MonadState s m, HasAuthorState s, HasDatabase s, MonadIO m) => m Text.Text
+bibliographyContent = do
+  urls <- use citedUrlSet
+  items <- mapM toBibTeXItem $ Set.toList urls
+  return $ Text.unlines $ items
+    
