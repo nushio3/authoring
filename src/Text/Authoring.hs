3104820f1df29ecbaa6310bfe2f4038087365a48
nushio <nushio@nushiohshio>
2013-10-09 17:16:14 +0000

    add global exporter module.

---
+++ src/Text/Authoring.hs
@@ -0,0 +1,14 @@
+module Text.Authoring (
+
+module Text.Authoring.Class,
+module Text.Authoring.Combinator,
+module Text.Authoring.Bibliography,
+module Text.Authoring.Label,
+  
+                      )
+where
+
+import Text.Authoring.Bibliography
+import Text.Authoring.Class
+import Text.Authoring.Combinator
+import Text.Authoring.Label
