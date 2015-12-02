33e1eafd10ef12204fd7bf9d4de59d1935ffa190
Takayuki Muranushi <muranushi@gmail.com>
2015-12-02 11:23:55 +0900

    comment on th.

--- src/Text/Authoring/TH.hs
+++ src/Text/Authoring/TH.hs
@@ -4,12 +4,12 @@
 
 {-|
 
-This module provides quasi-quotes 'rawQ' and 'escQ' for writing papers. 
-The quasi-quotes will generate authoring monads, so you can use them in authoring 
+This module provides quasi-quotes 'rawQ' and 'escQ' for writing papers.
+The quasi-quotes will generate authoring monads, so you can use them in authoring
 context like follows.
 
 > paragraph = do
->   let takahashi2007 = citep ["isbn:9784130627184"] 
+>   let takahashi2007 = citep ["isbn:9784130627184"]
 >       val = 3e6
 >   [rawQ| The dielectric strength of air is $ #{val} $ V/m @{takahashi2007}.  |]
 
@@ -17,7 +17,7 @@ We support antiquote syntax:
 
 >  #{val}
 
-for embedding values ('Show' instance is required), and
+for embedding values ('Show' instance is used), and
 
 >  @{...}
 
@@ -49,25 +49,25 @@ import Text.Authoring.Combinator.Writer (raw, esc)
 import Text.Authoring.Label (Label, fromValue)
 
 
--- | Quote with LaTeX special characters escaped. 
+-- | Quote with LaTeX special characters escaped.
 escQ = rawQ {quoteExp =  parseE (QQConfig { escaper = appE (varE 'esc)})}
 
 
--- | Quote without escaping any special characters.                                                           
+-- | Quote without escaping any special characters.
 rawQ :: QuasiQuoter
-rawQ = QuasiQuoter { 
+rawQ = QuasiQuoter {
   quoteExp = parseE (QQConfig { escaper = appE (varE 'raw)}),
-  quotePat  = error "Authoring QuasiQuotes are only for expression context" ,  
-  quoteType = error "Authoring QuasiQuotes are only for expression context" ,  
-  quoteDec  = error "Authoring QuasiQuotes are only for expression context" 
+  quotePat  = error "Authoring QuasiQuotes are only for expression context" ,
+  quoteType = error "Authoring QuasiQuotes are only for expression context" ,
+  quoteDec  = error "Authoring QuasiQuotes are only for expression context"
   }
 
--- | Quote a filename, without escaping any special characters.                                                
-inputQ :: QuasiQuoter                                                
-inputQ = quoteFile rawQ                                                
-                                                
-                                                
-data QQConfig = QQConfig 
+-- | Quote a filename, without escaping any special characters.
+inputQ :: QuasiQuoter
+inputQ = quoteFile rawQ
+
+
+data QQConfig = QQConfig
   { escaper :: ExpQ -> ExpQ }
 
 
@@ -75,7 +75,7 @@ parseE :: QQConfig -> String -> ExpQ
 parseE cfg str = do
   let res = parseString parseLang (Columns 0 0) str
   case res of
-    Failure xs -> do 
+    Failure xs -> do
       runIO $ do
         displayIO stdout $ renderPretty 0.8 80 $ xs <> linebreak
         putStrLn "Due to parse failure entire quote will be processed as a string."
@@ -85,16 +85,16 @@ parseE cfg str = do
 
 cvtE :: QQConfig -> Component -> ExpQ
 cvtE cfg (StrPart x)    = escaper cfg $ appE (varE 'T.pack) $ stringE x
-cvtE cfg (EmbedShow x)  = 
-  either (fallback "#" x) 
+cvtE cfg (EmbedShow x)  =
+  either (fallback "#" x)
          (escaper cfg . appE [| T.pack . showJoin |] . return) $
   Meta.parseExp x
-cvtE _   (EmbedMonad x) =  
+cvtE _   (EmbedMonad x) =
   either (fallback "@" x) return $
   Meta.parseExp x
 
 fallback :: String -> String -> String -> ExpQ
-fallback sym str _ = [| esc . T.pack |] `appE` 
+fallback sym str _ = [| esc . T.pack |] `appE`
                    (stringE $ printf "%s{%s}" sym str)
 
 trim :: String -> String
@@ -102,18 +102,18 @@ trim = T.unpack . T.strip . T.pack
 
 showJoin :: Show a => a -> String
 showJoin x = maybe sx id rsx
-  where 
+  where
     sx :: String
     sx = show x
     rsx :: Maybe String
     rsx = readMay sx
 
 joinE :: [ExpQ] -> ExpQ
-joinE = foldl ap [e| return () |] 
+joinE = foldl ap [e| return () |]
   where
     ap a b = appE (appE (varE '(>>) ) a ) b
 
-data Component 
+data Component
   = StrPart    String
   | EmbedMonad String
   | EmbedShow  String deriving (Eq,Show)
@@ -151,40 +151,37 @@ parseEmbedShow = EmbedShow <$> between (string "#{") (string "}") (some $ noneOf
 -- > myFormula :: Label
 -- > myFormula = fromValue MyFormula
 --
--- You can declare multiple labels at one shot by separating them with a comma. 
+-- You can declare multiple labels at one shot by separating them with a comma.
 --
 -- > [declareLabels| FluxConservation, FaradayLaw, GaussLaw, AmpereLaw |]
 
-declareLabels = 
- QuasiQuoter { 
-  quoteExp  = error "defineLabel QuasiQuote is only for declaration context" ,  
-  quotePat  = error "defineLabel QuasiQuote is only for declaration context" ,  
-  quoteType = error "defineLabel QuasiQuote is only for declaration context" ,  
+declareLabels =
+ QuasiQuoter {
+  quoteExp  = error "defineLabel QuasiQuote is only for declaration context" ,
+  quotePat  = error "defineLabel QuasiQuote is only for declaration context" ,
+  quoteType = error "defineLabel QuasiQuote is only for declaration context" ,
   quoteDec  = decLabelsQ
   }
- 
- 
-decLabelsQ :: String -> DecsQ 
+
+
+decLabelsQ :: String -> DecsQ
 decLabelsQ str = fmap concat $ mapM decLabelQ names
   where
-    names = 
+    names =
       map T.unpack $
       map T.strip $
       T.splitOn "," $ T.pack str
-              
-              
+
+
 decLabelQ :: String -> DecsQ
 decLabelQ theName = do
   let (hName:tName) = theName
       theTypeName = mkName (toUpper hName : tName)
       theConName  = mkName (toUpper hName : tName)
-      theValName  = mkName (toLower hName : tName)  
-  
-  let  decTheType = dataD (cxt []) theTypeName [] [normalC theConName []] [''Eq, ''Show, ''Typeable]                 
+      theValName  = mkName (toLower hName : tName)
+
+  let  decTheType = dataD (cxt []) theTypeName [] [normalC theConName []] [''Eq, ''Show, ''Typeable]
        typeTheVal = sigD theValName (conT ''Label)
        decTheVal = funD theValName [clause [] (normalB theBody) []]
        theBody = appE (varE 'fromValue) (conE theConName)
   sequence [decTheType, typeTheVal, decTheVal]
-  
-
-
